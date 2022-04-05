74e927fae657e850415d2c6564e21ff074d5a72d
butrint24 <butrint.salihu24@gmail.com>
2022-04-05 13:29:56 +0200

    annotate

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PlatformService.Models
 {
     public class PlatformService
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Publisher { get; set; }
