7229e846dc327416d0ce56e549b9679b3bb3c91a
butrint24 <butrint.salihu24@gmail.com>
2022-04-20 15:21:47 +0200

    Data Layer On Command Service Completed

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -23,11 +23,11 @@ namespace CommandsService
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
